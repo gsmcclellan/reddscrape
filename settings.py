b541f827705065e33098463869c920e8e29fc6ea
Greg McClellan <gsmcclellan@gmail.com>
2014-01-13 15:41:09 -0800

    First commit

---
+++ settings.py
@@ -0,0 +1,34 @@
+import os, sys
+
+
+# Path to save images
+dirpath = os.path.abspath(os.path.dirname(__file__))
+
+
+# List of subreddits to scrape
+subreddits = [
+	'AdviceAnimals',
+	'AubreyPlaza',
+	'aww',
+	'BearsWithBeaks',
+	'christinahendricks',
+	'Damnthatsinteresting',
+	'firefly',
+	'firstworldanarchists',
+	'funny',
+	'futureporn',
+	'gaming',
+	'HybridAnimals',
+	'Offensive_Wallpapers',
+	'OldSchoolCool',
+	'pics',
+	'pictureswithpatrick',
+	'randomsexiness',
+	'SpecArt',
+	'thebookofpureevil',
+	'TreesSuckingAtThings',
+	'wallpapers',
+	'WatchItForThePlot',
+	'wheredidthesodago',
+	'woahdude',
+	]
